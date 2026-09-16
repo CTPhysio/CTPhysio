6ea8b661fd8ef67de7f40eb751a0574f670cd324
CTPhysio <chris@christileyphysiotherapy.com>
2026-09-16 14:39:34 +0100

    Updated Team.tsx

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -178,13 +178,13 @@ const Team: React.FC = () => {
               <div className="bg-gradient-to-br from-sky-50 to-slate-100 px-6 pt-8 pb-0 flex justify-center">
                 <div className="w-40 h-40 sm:w-48 sm:h-48 rounded-full border-4 border-white shadow-[0_4px_20px_rgba(15,23,42,0.14)] overflow-hidden">
                   <img
-                    src="/Headshot_2022.png"
+                    src="/Chris_Headshot_No_Background.png"
                     alt="Chris Tiley - Founder & Physiotherapist"
                     style={{
                       width: '100%',
-                      height: '130%',
+                      height: '100%',
                       objectFit: 'cover',
-                      objectPosition: 'center 5%',
+                      objectPosition: 'center top',
                       display: 'block',
                     }}
                   />
