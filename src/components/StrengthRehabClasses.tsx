856954788aece5eaea4d45e88b1d84162d789ad2
CTPhysio <chris@christileyphysiotherapy.com>
2026-09-25 12:12:03 +0100

    Updated StrengthRehabClasses.tsx

--- src/components/StrengthRehabClasses.tsx
+++ src/components/StrengthRehabClasses.tsx
@@ -15,7 +15,7 @@ const classTimetable: ClassSlot[] = [
   { day: 'Monday', time: '6:30pm', spaces: 2 },
   { day: 'Monday', time: '7:30pm', spaces: 0 },
   { day: 'Wednesday', time: '6:30pm', spaces: 0 },
-  { day: 'Wednesday', time: '7:30pm', spaces: 0 },
+  { day: 'Wednesday', time: '7:30pm', spaces: 1 },
 ];
 
 const DiscoveryButton: React.FC<{ className?: string; children?: React.ReactNode }> = ({
